2c3eb4aff5c70ec1e0f9447c46e4bd17bee100c9
Francesco Strozzi <francesco.strozzi@gmail.com>
2012-01-02 15:30:18 +0100

    added file check and quality conversion

--- ext/faster.c
+++ ext/faster.c
@@ -11,33 +11,45 @@ Copyright(C) Francesco Strozzi <francesco.strozzi@gmail.com>
 KSEQ_INIT(gzFile, gzread)
 
 static VALUE method_parse(VALUE self, VALUE file) {
-	VALUE filename = StringValue(file);
+
 	gzFile fp;
-	FILE *check_file;
-	if ((check_file = fopen(RSTRING_PTR(filename), "r+")) == NULL) {
-			printf("No file found!");
+	if (!(fp = gzopen(RSTRING_PTR(file), "r"))) {
+		rb_raise(rb_eArgError,"File %s not found!", RSTRING_PTR(file));
 	}
 	else {
-		fclose(check_file);
-		fp = gzopen(RSTRING_PTR(filename), "r");
+		
 		kseq_t *seq;
 		seq = kseq_init(fp);
+
 		while (kseq_read(seq) >= 0) {
+
 			VALUE arr = rb_ary_new();
 			rb_ary_push(arr, rb_str_new2(seq->name.s));
 			if (seq->comment.l) rb_ary_push(arr, rb_str_new2(seq->comment.s));
 			rb_ary_push(arr, rb_str_new2(seq->seq.s));
-			if (seq->qual.l) rb_ary_push(arr, rb_str_new2(seq->qual.s));
+
+			if (seq->qual.l) {
+
+				VALUE rb_quality = rb_ary_new();
+
+				int unsigned i = 0;
+				while(i < seq->qual.l) {
+					rb_ary_push(rb_quality,INT2FIX(*(seq->qual.s + i) - 33));
+					i++;
+				}
+
+				rb_ary_push(arr,rb_quality);
+			}
+
 			rb_yield(arr);
 		}
+
 		kseq_destroy(seq);
 		gzclose(fp);
 		return Qtrue;
 	}
-		
-
 }
-
+ 
 void Init_faster() {
 	VALUE Faster = rb_define_module("Faster");
 	rb_define_singleton_method(Faster,"parse",method_parse,1);
