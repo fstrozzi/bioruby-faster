4f5781a9f530429968d4ad69a113f42a406b4390
Francesco Strozzi <francesco.strozzi@gmail.com>
2012-03-08 13:15:31 +0100

    updated Struct and parse_fastq method

--- lib/bio/faster.rb
+++ lib/bio/faster.rb
@@ -17,7 +17,9 @@ module Bio
              :seq, :pointer,
              :quality, :pointer,
              :filename, :pointer,
-             :stream, :pointer
+             :stream, :pointer,
+             :line, :pointer,
+             :bad_chars, :pointer
     end
 
     attach_function :fastQ_iterator, [Record], :int
@@ -25,8 +27,11 @@ module Bio
     def self.parse_fastq(file)
         record = Record.new
         record[:filename] = FFI::MemoryPointer.from_string(file)
-        while Bio::Faster.fastQ_iterator(record) == 1
+        while (ret = Bio::Faster.fastQ_iterator(record)) == 1
           yield [record[:id].read_string,record[:seq].read_string, record[:quality].read_string]
+        end
+        if ret == -1
+          raise RuntimeError, "Bad formatted FastQ file!"
         end
     end
 
