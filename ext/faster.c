4871a3d924ac5b075a390d2f85e11b413b3813e7
Francesco Strozzi <francesco.strozzi@gmail.com>
2012-03-08 13:15:05 +0100

    added functions for file format checks

--- ext/faster.c
+++ ext/faster.c
@@ -15,6 +15,8 @@ typedef struct {
     char *seq;
     char *quality;
     char *filename;
+    char *line;
+    char *bad_chars;
     FILE *stream;
 
 }Record;
@@ -30,30 +32,50 @@ alloc_and_copy(char *dst, char *src) {
   return dst;
 }
 
-/*
-int main() {
 
-Record *test_seq;
+int check_bad_chars(char *invalid_chars, char *string_to_check) {
 
-fastQ_iterator(&test_seq);
+   char *c = string_to_check;
+   while (*c) {
+       if (strchr(invalid_chars, *c))
+          return 1;
+       c++;
+   }
+
+   return 0;
 }
-*/
+
+int check_header(char *header, char *firstline) {
+    if (*header == *firstline)
+        return 1;
+    else {
+        return 0;
+    }
+}
 
 int fastQ_iterator(Record *seq) {
 
     if (!seq->stream)
       seq->stream = fopen(seq->filename,"r");
+    if (!seq->line)
+      seq->line = malloc(sizeof (char)* _BSIZE);
+    if (!seq->bad_chars)
+      seq->bad_chars = " \x1F\x7F\t\v\e";
 
-    char line[_BSIZE];
-
+    char *header = "@";
     for (int i = 0; i < 4; i++)
     {
-      if (fgets(line, _BSIZE, seq->stream) == NULL) return 0;
-      char *value;
-      if (i==0) seq->id = alloc_and_copy(seq->id, line);
-      if (i==1) seq->seq = alloc_and_copy(seq->seq, line);
-      if (i==3) seq->quality = alloc_and_copy(seq->quality, line);
+      if (fgets(seq->line, _BSIZE, seq->stream) == NULL) return 0;
 
+      if (i==0) {
+        if (!check_header(header,seq->line)) return -1;
+        seq->id = alloc_and_copy(seq->id, seq->line);
+      }
+      else {
+        if (check_bad_chars(seq->bad_chars,seq->line)) return -1;
+        if (i==1) seq->seq = alloc_and_copy(seq->seq, seq->line);
+        if (i==3) seq->quality = alloc_and_copy(seq->quality, seq->line);
+      }
     }
 
     return 1;
