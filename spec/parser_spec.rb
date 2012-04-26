f839acad13332cb53b5b9f8e35d17e85a0162e10
Francesco Strozzi <francesco.strozzi@gmail.com>
2012-04-26 17:16:11 +0200

    updated specs

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -10,6 +10,22 @@ describe Bio::Faster do
       end
     end
 
+
+    it "should handle correctly Phred64 qualities (Solexa)" do
+      file = TEST_DATA+"/formats/misc_rna_as_solexa.fastq"
+      bioruby_data = []
+      Bio::FlatFile.open(File.open(file)).each_entry do |seq|
+        seq.format = "fastq-solexa"
+        bioruby_data << [seq.entry_id,seq.seq,seq.qualities]
+      end
+      faster_data = []
+      Bio::Faster.new(file, :solexa).each_record do |seq|
+        seq[0] = seq[0].split(" ").first
+        faster_data << seq
+      end
+      faster_data.should == bioruby_data
+    end
+    
     it "should read different FastQ formats" do
       files = Dir.glob(TEST_DATA+"/formats/*.fastq")
       files.each do |file|
@@ -25,21 +41,6 @@ describe Bio::Faster do
         faster_data.should == bioruby_data
       end
 
-    end
-
-    it "should handle correctly Phred64 qualities (Solexa)" do
-      file = TEST_DATA+"/formats/misc_rna_as_solexa.fastq"
-      bioruby_data = []
-      Bio::FlatFile.open(File.open(file)).each_entry do |seq|
-        seq.format = "fastq-solexa"
-        bioruby_data << [seq.entry_id,seq.seq,seq.qualities]
-      end
-      faster_data = []
-      Bio::Faster.new(file, :solexa).each_record do |seq|
-        seq[0] = seq[0].split(" ").first
-        faster_data << seq
-      end
-      faster_data.should == bioruby_data
     end
 
 
