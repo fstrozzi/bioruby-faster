3a4a1b88a8dc731f681bfef541592b6f6460f614
Francesco Strozzi <francesco.strozzi@gmail.com>
2012-04-27 12:02:55 +0200

    fixed files being loaded in different order

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -43,14 +43,14 @@ describe Bio::Faster do
     end
 
     it "can read also gzipped FastQ files" do
-      gz_files = Dir.glob(TEST_DATA+"/formats/gzipped/*.fastq.gz")
+      gz_files = Dir.glob(TEST_DATA+"/formats/gzipped/*.fastq.gz").sort
       gz_seq = []
       gz_files.each do |file|
         Bio::Faster.new(file).each_record do |seq|
            gz_seq << seq
         end    
       end
-      files = Dir.glob(TEST_DATA+"/formats/gzipped/*.fastq")
+      files = Dir.glob(TEST_DATA+"/formats/gzipped/*.fastq").sort
       norm_seq = []
       files.each do |file|
         Bio::Faster.new(file).each_record do |seq|
